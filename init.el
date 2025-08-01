8f4b0e979d25eda3050a1a7fe6577dfd351d8cec
Romero-rgb <aromero94@proton.me>
2025-08-01 21:31:22 +0200

    initial configuration

--- init.el
+++ init.el
@@ -1,10 +1,26 @@
-;; Load a dark mode theme
-(load-theme 'deeper-blue t)
+;; package straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; Disable menu bar, scroll bar, and tool bar
+;; Disable menu bar, scroll bar, and tool bar, and splash screen
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+(setq inhibit-splash-screen t)
+(setq use-file-dialog nil)
 
 (setq gc-cons-threshold 100000000) ; 100 mb
 (setq read-process-output-max (* 1024 1024)) ; 1mb
@@ -29,6 +45,10 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
+(setq straight-use-package-by-default t)
+(setq use-package-always-defer t)
+
+
 ;; You'll be installing your packages with the
 ;; built-in package.el script
 (require 'package)
@@ -48,6 +68,26 @@
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
+;; In confirmation dialogs, we want to be able to type y and n instead of having;; to spell the whole words:
+
+(use-package emacs
+  :init
+  (defalias 'yes-or-no-p 'y-or-n-p))
+
+;; Make everything use UTF-8:
+
+(use-package emacs
+  :init
+  (set-charset-priority 'unicode)
+  (setq locale-coding-system 'utf-8
+        coding-system-for-read 'utf-8
+        coding-system-for-write 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8)
+  (set-selection-coding-system 'utf-8)
+  (prefer-coding-system 'utf-8)
+  (setq default-process-coding-system '(utf-8-unix . utf-8-unix)))
+
 
 
 (use-package vertico
@@ -64,3 +104,32 @@
 (use-package savehist
   :init
   (savehist-mode))
+
+(use-package emacs
+  :init
+  (set-face-attribute 'default nil
+    :font "PragmataPro Mono Liga"
+    :height 160))
+
+(use-package doom-themes
+  :demand
+  :config
+  (load-theme 'doom-solarized-dark t))
+
+(use-package emacs
+  :init
+  (defun ab/enable-line-numbers ()
+    "Enable relative line numbers"
+    (interactive)
+    (display-line-numbers-mode)
+    (setq display-line-numbers 'relative))
+  (add-hook 'prog-mode-hook #'ab/enable-line-numbers))
+
+use-package doom-modeline
+  :ensure t
+:init (doom-modeline-mode 1))
+
+(use-package nerd-icons)
+
+
+
